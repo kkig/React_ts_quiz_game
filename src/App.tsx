c7d64d276f412b5c7ad1d5db8f07195b74bfef8d
kkig <kigoshi.kanazawa@gmail.com>
2020-08-18 22:42:18 +0900

    question card display when not loading and not gameover

--- src/App.tsx
+++ src/App.tsx
@@ -69,14 +69,16 @@ const App = () => {
 
       {!gameOver ? <p className="score">Score:</p> : null }
       {loading ? <p>Loading Questions...</p> : null}
-      {/* <QuestionCards 
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
+      { !loading && !gameOver && (
+        <QuestionCards 
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
