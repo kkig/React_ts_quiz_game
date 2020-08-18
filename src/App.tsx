1d745510cbc15a65a1933bf6919786fb0b42c5dd
kkig <kigoshi.kanazawa@gmail.com>
2020-08-18 22:47:40 +0900

    next question btn ternary

--- src/App.tsx
+++ src/App.tsx
@@ -79,9 +79,17 @@ const App = () => {
           callback={checkAnswer}
         />
       )}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+
+      { !gameOver && 
+        !loading && 
+        userAnswers.length === number + 1 && 
+        number !== TOTAL_QUESTIONS - 1 ? 
+        (
+          <button className="next" onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null }
+
     </div>
   );
 }
